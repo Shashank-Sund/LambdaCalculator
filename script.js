fe621a99f146f1459adb862be4705cade1d94706
Shashank-Sund <shanksund@gmail.com>
2023-09-05 14:49:05 -0400

    Update script.js
    added send to db functionality (still needs to be fixed)

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     const characterForm = document.getElementById('characterForm');
     const responseDiv = document.getElementById('response');
+    const saveButton = document.getElementById('saveButton');
 
+    // SOLVE button logic
     characterForm.addEventListener('submit', function(event) {
         event.preventDefault(); // Prevent the default form submission behavior
 
@@ -40,5 +42,50 @@ document.addEventListener('DOMContentLoaded', function() {
         // Send the POST request with the JSON data
         xhr.send(JSON.stringify(characters));
     });
+    
+    // SAVE button logic (FIXME: POST request is failing at network level. 
+    // API gateway works in backend, script is not able to send a request and persist
+    // data to dynamodb )
+    saveButton.addEventListener("click", function(event) {
+        event.preventDefault(); // Prevent the default form submission behavior
+
+        // Get the input value
+        const charactersInput = document.getElementById('characters');
+        const characters = {"equation": charactersInput.value};
+
+        // Define the API endpoint
+        const url = 'https://3jogrytmgj.execute-api.us-east-1.amazonaws.com/Prod2'; // Replace with your actual API endpoint
+
+        // Create a new XMLHttpRequest object
+        const xhr2 = new XMLHttpRequest();
+
+        // Configure the POST request (Post requst, url with CORS enabled, Synchronous)
+        xhr2.open('POST', url, true); // FIXME: change to false?
+        xhr2.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+        // Handle the response from the server
+        // Waits for request to be finished and response is ready
+        xhr2.onreadystatechange = function() {
+            if (xhr2.readyState == 4 && xhr2.status === 200) {
+                const response = JSON.parse(xhr2.responseText);
+                responseDiv.textContent = `it worked.`;
+            } else {
+                console.error('Request failed with status:', xhr2.status);
+                responseDiv.textContent = 'An error occurred while sending the request: ' + xhr2.status;
+            }
+        };
+
+        // Handle network errors
+        xhr2.onerror = function() {
+            console.error('Network error occurred');
+            responseDiv.textContent = 'A network error has occurred.';
+        };
+
+        // Send the POST request with the JSON data
+        xhr2.send(JSON.stringify(characters));
+    });
+
+
+
 });
 
